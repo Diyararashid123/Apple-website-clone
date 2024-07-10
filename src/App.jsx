2d6974011fbb53565726c2c9ab78471078cbedaf
Diyara <diyararashid@gmail.com>
2024-07-10 17:49:23 +0200

    implement the structure for the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+const App = () => {
+
+
+  return (
+   
+    <main className='bg-black'> 
+    Navbar
+    Hero
+    Highlights
+    </main>
+  )
+}
+
+export default App
